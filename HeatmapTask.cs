dcde6675d298cdcff1a891a10ecb2496eec06ee4
Stanislav Saveliev <texnikum2288@gmail.com>
2024-12-12 22:05:56 +0300

    Добавьте файлы проекта.

---
+++ HeatmapTask.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var daysInMonth = 31;
+            var monthsInYear = 12;
+            var birthsPerDate = InitializeBirthsPerDate(daysInMonth - 1, monthsInYear);
+            var dayLabels = GenerateLabels(2, daysInMonth - 1);
+            var monthLabels = GenerateLabels(1, monthsInYear);
+
+            foreach (var name in names)
+            {
+                if (name.BirthDate.Day != 1)
+                {
+                    birthsPerDate[name.BirthDate.Day - 2, name.BirthDate.Month - 1]++;
+                }
+            }
+
+            return new HeatmapData(
+                "Тепловая карта рождаемости по дням и месяцам",
+                birthsPerDate,
+                dayLabels,
+                monthLabels);
+        }
+
+        private static double[,] InitializeBirthsPerDate(int days, int months)
+        {
+            return new double[days, months];
+        }
+
+        private static string[] GenerateLabels(int start, int count)
+        {
+            return Enumerable.Range(start, count).Select(number => number.ToString()).ToArray();
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,38 @@
+﻿namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var days = InitializeDaysArray();
+            var birthsCounts = new double[31];
+            for (int i = 0; i < birthsCounts.Length; i++)
+            {
+                birthsCounts[i] = 0;
+            }
+
+            foreach (var nameData in names)
+            {
+                if (nameData.Name == name && nameData.BirthDate.Day != 1)
+                {
+                    birthsCounts[nameData.BirthDate.Day - 1]++;
+                }
+            }
+
+            return new HistogramData(
+                $"Рождаемость людей с именем '{name}'",
+                days,
+                birthsCounts);
+        }
+
+        private static string[] InitializeDaysArray()
+        {
+            var days = new string[31];
+            for (int i = 0; i < days.Length; i++)
+            {
+                days[i] = (i + 1).ToString();
+            }
+            return days;
+        }
+    }
+}
